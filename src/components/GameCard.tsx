df491d8751241b4cbed58aa66e5dc39c82f7687f
yogeshbpatil <patilyogeshkumarb@gmail.com>
2024-03-07 12:19:41 +0530

    remove duplicated styles 8.16

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <Card width="300px" borderRadius={10} overflow="hidden">
+    <Card>
       {/* <Image src={getCroppedImageUrl(game.background_image)} /> */}
       <Image src={game.background_image} />
       <CardBody>
