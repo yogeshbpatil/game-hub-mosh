7f6dad23af8d29b42db318bbb9a1b2688f492551
yogeshbpatil <patilyogeshkumarb@gmail.com>
2024-03-16 13:37:21 +0530

    sort game seoncd file add 8.27

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -19,7 +19,8 @@ const useGames = (
 )=> useData<Game>('/games', {
   params: {
     genre: gameQuery.genre?.id,
-    platforms: gameQuery.platform?.id
+    platforms: gameQuery.platform?.id,
+    ordering: gameQuery.sortOrder
   }},
   [gameQuery]);
 export default useGames; 
